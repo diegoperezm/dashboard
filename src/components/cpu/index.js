81e2643e4229fc9056107ad8ae964979c2a4eb60
diegoperezm <diegoperezcorreo@gmail.com>
2023-07-20 18:46:33 -0400

    Add cpu, disco graph

--- src/components/cpu/index.js
+++ src/components/cpu/index.js
@@ -1,9 +1,72 @@
-import React from "react";
-export default function Cpu() {
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-  return (
-    <div className="mb-5 canvas-container">
+const data = [
+  { idle: 2.0 },
+  { idle: 48.24 },
+  { idle: 38.74 },
+  { idle: 97.75 },
+  { idle: 70.99 },
+  { idle: 98.48 },
+  { idle: 78.74 },
+  { idle: 98.25 },
+  { idle: 88.99 },
+  { idle: 96.97 },
+  { idle: 98.31 },
+  { idle: 98.25 },
+  { idle: 88.99 },
+  { idle: 96.97 },
+  { idle: 98.31 },
+  { idle: 98.25 },
+  { idle: 88.99 },
+  { idle: 96.97 },
+  { idle: 98.31 },
+];
 
-    </div>
-  );
+export default class Cpu extends PureComponent {
+  render() {
+    return (
+      <>
+        <div className="mt-5  canvas-container">
+          <h1 align="center">CPU (idle)</h1>
+          {/* width="90%" height="90%" problema: 
+            el componente  desborda la Página */}
+          <ResponsiveContainer width="80%" height="80%">
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="idle"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </>
+    );
+  }
 }
--- src/components/disco/index.js
+++ src/components/disco/index.js
@@ -1,56 +1,71 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const data = [
-  { name: 'Used', value: 17 },
-  { name: 'Available', value: 192 },
+  { name: "Used", value: 17 },
+  { name: "Available", value: 192 },
 ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
       {`${(percent * 100).toFixed(0)}%`}
     </text>
   );
 };
 
-export default class  Disco extends PureComponent {
-
+export default class Disco extends PureComponent {
   render() {
     return (
       <>
         <div className="mt-5  canvas-container">
-          <h1 align="center">Espacio en /dev/<span>sda1</span></h1>
-      <ResponsiveContainer width="85%" height="85%">
-        <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={180}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-
+          <h1 align="center">
+            Espacio en /dev/<span>sda1</span>
+          </h1>
+          <ResponsiveContainer width="85%" height="85%">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={180}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
-</>
+      </>
     );
   }
-}     
-     
-     
+}
